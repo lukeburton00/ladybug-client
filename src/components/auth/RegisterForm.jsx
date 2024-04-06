75a2fd8f945e94ce8698a77fb0adfdf00934010e
Luke Burton <lukesburton19@gmail.com>
2024-04-05 18:54:05 -0600

    add registration page

---
+++ src/components/auth/RegisterForm.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import instance from '../../utils/axiosConfig';
+
+function RegisterPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password_confirmation, setPasswordConfirmation] = useState('');
+  const navigate = useNavigate();
+
+  const handleRegister = async () => {
+    try {
+      await instance.post('/api/auth/register', {
+        user: {
+          email,
+          password,
+          password_confirmation
+        },
+        withCredentials: true,
+      });
+      navigate('/dashboard');
+    } 
+    
+    catch (error) {
+      console.error('Register failed:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    <input
+        type="password"
+        placeholder="Password Confirmation"
+        value={password_confirmation}
+        onChange={(e) => setPasswordConfirmation(e.target.value)}
+    />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+}
+
+export default RegisterPage;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Brows
 import Home from './components/pages/Home';
 import Navbar from './components/nav/Navbar.jsx';
 import LoginForm from './components/auth/LoginForm.jsx';
+import RegisterPage from './components/auth/RegisterForm.jsx';
 import Dashboard from './components/dashboard/Dashboard';
 import './index.css';
 
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
