90ca4d7f2ffc848eb01f3d1593a75dbe95ef117d
Luke Burton <lukesburton19@gmail.com>
2024-04-22 18:48:34 -0600

    update dashboard to show, create, and delete projects

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios';
+import instance from '../../utils/axiosConfig';
+import { useNavigate } from 'react-router-dom'
 import LogoutForm from '../auth/LogoutForm';
 function Dashboard() {
   const [data, setData] = useState('');
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    console.log('Dashboard');
     const getProjects = async () => {
       try {
-        const response = await axios.get('http://localhost:5001/api/projects/', {withCredentials: true});
+        const response = await instance.get('/projects/', {withCredentials: true});
         setData(response.data);
         console.log(response.data);
       }
@@ -26,15 +27,57 @@ function Dashboard() {
     
     getProjects();
   }, []);
-  
+
+    const handleCreateProject = async () => {
+        navigate('/create-project');        
+    }
+
+    const handleDeleteProject = async (projectId) => {
+        try {
+            await instance.delete(`/projects/${projectId}`);
+            const response = await instance.get('/projects/', {withCredentials: true});
+            setData(response.data);
+            navigate('/dashboard');
+        }
+
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+    
+    const handleNavigateToProject = async (projectName) => {
+        try {
+            navigate(`/projects/${projectName}`);
+        }
+
+        catch (error) {
+            console.log(error);      
+        }
+    }
+
+
   if (loading) return <div>Loading...</div>;
 
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div>
-      <p>{JSON.stringify(data)}</p>
-      <LogoutForm />
+    <div class="container">
+      <div>
+        <h1 class="py-5 my-2">Projects</h1>
+        {data.projects.map(project => 
+            <div class="row p-3">
+                <div class="w-25 p-2 rounded shadow">
+                    <p class="fw-bold">{project.name}</p>
+                    <button class="btn btn-danger m-1" onClick={() => handleDeleteProject(project.id)}>Delete</button>
+                    <button class="btn btn-primary m-1" onClick={() => handleNavigateToProject(project)}>Open</button>
+                </div>
+            </div>
+        )}
+        <div class="row p-3">
+            <button class="btn btn-primary w-25 shadow-lg rounded" onClick={handleCreateProject}>Create Project</button>
+        </div>
+      </div>
     </div>
   );
 }
