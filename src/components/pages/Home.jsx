d74d8d835690e559f16ed64361cfbc9c6f844310
Luke Burton <lukesburton19@gmail.com>
2024-04-22 18:46:31 -0600

    update landing page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,36 @@
+import hero from '../../assets/hero.jpg'
+import team from '../../assets/team.jpg'
+
 function Home() {
   return (
     <div>
-      <h1>Welcome to My Website!</h1>
-      <p>This is the landing page content.</p>
+        <div class="container display-flex justify-content-center align-items-center "> 
+            <div class="row-sm my-5 py-5 text-center">
+                <h1 class="display-1 fw-bold display-inline">Track your projects with ease.</h1>
+            </div>
+            <div class="row">
+                <a href="/login" class="container w-25 align-items-center justify-content-center text-center btn btn-primary">Get Started</a>
+            </div>
+            <div class="row container justify-content-center align-items-center my-5 py-5">
+                <div class="col-md">
+                    <h2 class="display-4 text-start fw-bold pb-4">Convenient task management</h2>
+                    <p class="text-start fw-bold col-4">Track your upcoming, current, and completed tasks on a simple and intuitive Task Board.</p>
+                </div>
+                <div class="col-md">
+                    <img src={hero} width="600" height="400" class="col"/>
+                </div>
+            </div>
+            <div class="row container justify-content-center align-items-center my-5 py-5">
+               <div class="col-md">
+                    <img src={team} width="600" height="400" class="col"/>
+                </div>
+                <div class="col-md align-items-right justify-content-right">
+                    <h2 class="display-4 fw-bold text-end pb-4">Collaborate Seamlessly</h2>
+                    <p class="text-end fw-bold col-4 ms-auto">Adding new project members is as easy as sending an email.</p>
+                </div>
+            </div>
+        </div>
+        <p>This is the landing page content.</p>
     </div>
   );
 }
