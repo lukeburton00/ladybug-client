2abdbd39a6cfccfe6608a673e794005c6210bda6
Luke Burton <lukesburton19@gmail.com>
2024-06-11 14:55:58 -0600

    add deletion of tasks

--- src/components/projects/ProjectPage.jsx
+++ src/components/projects/ProjectPage.jsx
@@ -48,6 +48,21 @@ function ProjectPage() {
       navigate(`/projects/${id}/create-task`);  
     };
 
+    const handleDeleteTask = async (taskId) => {
+        try {
+            await instance.delete(`/projects/${id}/tasks/${taskId}`, {withCredentials: true});
+            var response = await instance.get(`/projects/${id}`, {withCredentials: true});
+            setProject(response.data);
+            response = await instance.get(`/projects/${id}/tasks`, {withCredentials: true});
+            setTasks(response.data);
+            navigate(`/projects/${id}/`);
+        }
+    
+        catch (error) {
+            console.error('Delete task failed:', error);
+        }
+      };
+
     const onDragEnd = (result) => {
         console.log("Drag End");
     }
@@ -64,7 +79,7 @@ function ProjectPage() {
         return (
             <div class="row p-3">
                 <div class="w-25 p-2 rounded shadow">
-                    <p class="fw-bold">{task.title}</p>
+                    <p>{task.title}</p>
                     <button class="btn btn-danger m-1" onClick={() => handleDeleteTask(task.id)}>Delete</button>
                     <button class="btn btn-primary m-1" onClick={() => handleNavigateToTask(task.id)}>Open</button>
                 </div>
@@ -75,7 +90,7 @@ function ProjectPage() {
     return (
             <div class="container m-5">
                 <h1 class="display-5"> {project.project.name}</h1>
-                <h1 class="display-5"> {tasksList}</h1>
+                <h4 class="">{tasksList}</h4>
                 <button class="btn btn-primary w-25 shadow-lg rounded" onClick={handleCreateTask}>Create Task</button>
             </div>
     )
